2a2d0bf764b3895c43c6fbe5ec5c025ea5756711
Jeffree27 <2015081592@ust-ics.mygbiz.com>
2018-09-09 14:10:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jeffree/nietojeffreelab2/constraintlayout.java
@@ -0,0 +1,13 @@
+package com.example.jeffree.nietojeffreelab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class constraintlayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraintlayout);
+    }
+}
